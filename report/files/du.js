47e65fe897d8c98c93c99b0f48e1546dcd69b922
TheRA1A <rajashubham366@gmail.com>
2024-10-17 10:21:49 +0530

    router project

---
+++ report/files/du.js
@@ -0,0 +1,2 @@
+var g_data = {"data":[{"n":"work.router_pkg","id":3,"zf":1,"tc":85.18,"g":70.37,"a":100.00},{"n":"work.top","id":4,"zf":1,"tc":61.11,"d":77.77,"a":44.44}]};
+processDuData(g_data);
---
+++ report/files/dulink.js
@@ -0,0 +1,2 @@
+var g_data = {"3":["work.router_pkg",85.18,1],"4":["work.top",61.11,1]};
+processDuLinks(g_data);
